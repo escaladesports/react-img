70c50c8282106348f53f864a319d74e8ce4d75dc
Kennedy Rose <kennedy@kennedyrose.com>
2017-11-20 14:17:42 -0600

    Boilerplate

---
+++ dist-modules/demo.js
@@ -0,0 +1,10 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+var demo = function demo() {
+  return 'demo';
+};
+
+exports.default = demo;
---
+++ dist-modules/index.js
@@ -0,0 +1,6 @@
+'use strict';
+
+// this should be the entry point to your library
+module.exports = {
+  demo: require('./demo').default
+};
